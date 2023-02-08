ebaac9649608bc2478643d173313d144cdb542cf
Vic Mortelmans <vic@mortelmans.com>
2023-02-08 14:01:12 +0000

    Wiezen

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const host = '127.0.0.1'
 const pug = require("pug")
 const WACHTEND = "wachtend"
 const AANMELDEND = "aanmeldend"
+const SPELEND = "spelend"
+const GEKICKT = "gekickt"
 let clients = []
 let aantal = 0
 class Player {
@@ -18,49 +20,124 @@ class Player {
         this.status = AANMELDEND
     }
 }
-
 const WebSocketServer = require('ws')
 const wss = new WebSocketServer.Server({
     port: ws_port,
     host: host
 })
+
+function scherm_wachtend_of_aanmeldend_sturen() {
+    for (const p of clients) {
+        let message = {}
+        if (p.status === WACHTEND) {
+            message.htmlFragment = pug.renderFile("views/wachtend.pug", {
+                aantal: aantal,
+                naam: p.naam,
+                clients: clients.filter(p => p.naam)
+            })
+        }
+        else if (p.status === AANMELDEND) {
+            message.htmlFragment = pug.renderFile("views/aanmelden.pug", {
+                aantal: aantal
+            })
+        }
+        else if (p.status === SPELEND) {
+            message.htmlFragment = pug.renderFile("views/starten.pug", {
+                clients: clients.filter(p => p.naam)
+            })
+        }
+        else if (p.status === GEKICKT) {
+            message.htmlFragment = pug.renderFile("views/gekick.pug")
+        }
+        message.id = "content"
+        p.ws.send(JSON.stringify(message))
+    }
+}
 wss.on("connection", ws => {
     let player = new Player(ws)
     clients.push(player)
+    if (aantal === 4) {
+        player.status = GEKICKT
+        scherm_wachtend_of_aanmeldend_sturen()
+        return
+    }
     let message = {}
     message.htmlFragment = pug.renderFile("views/aanmelden.pug", { aantal: aantal })
     message.id = "content"
     ws.send(JSON.stringify(message))
     ws.on("message", data => {
-        aantal = aantal + 1
-        player.naam = data
-        player.status = WACHTEND
-        for (const p of clients) {
-            let message = {}
-            if (p.status === WACHTEND) {
-                message.htmlFragment = pug.renderFile("views/wachtend.pug", {
-                    aantal: aantal,
-                    naam: p.naam,
-                    clients: clients
-                })
-            }
-            else if (p.status === AANMELDEND) {
-                message.htmlFragment = pug.renderFile("views/aanmelden.pug", {
-                    aantal: aantal
-                })
-            }
-            message.id = "content"
-            p.ws.send(JSON.stringify(message))
+        if (aantal === 4) {
+            player.status = GEKICKT
+            scherm_wachtend_of_aanmeldend_sturen()
+            return
         }
+        let naam = data.toString().trim()
+        if (naam === "") {
+            let message = {}
+            message.htmlFragment = pug.renderFile("views/aanmelden.pug", {
+                aantal: aantal,
+                message: "Er is geen naam ingevoerd."
+            })
+            message.id = "content"
+            ws.send(JSON.stringify(message))
+            return
+        }
+        let i
+        for (i in clients) {
+            if (naam === clients[i].naam) {
+                //foutmelding geven
+                let message = {}
+                message.htmlFragment = pug.renderFile("views/aanmelden.pug", {
+                    aantal: aantal,
+                    message: "Deze naam is al in gebruik."
+                })
+                message.id = "content"
+                ws.send(JSON.stringify(message))
+                return
+            }
+        }
+        aantal = aantal + 1
+        player.naam = naam
+        if (aantal === 4) {
+            for (let p of clients) {
+                if (p.status === WACHTEND) {
+                    p.status = SPELEND
+                }
+            }
+            player.status = SPELEND
+        }
+        else if (aantal < 4) {
+            player.status = WACHTEND
+        }
+        scherm_wachtend_of_aanmeldend_sturen()
+
     })
 
     ws.on("close", () => {
-        for (const [key, value] of Object.entries(clients)) {
-            if (ws === value) {
-                aantal = aantal - 1
-                delete clients[key]
+        let i
+        for (i in clients) {
+            if (ws === clients[i].ws) {
+                if (clients[i].status === WACHTEND) {
+                    aantal = aantal - 1
+                }
+                else if (clients[i].status === SPELEND) {
+                    aantal = aantal - 1
+                    let j
+                    for (j in clients) {
+                        if (clients[j].status === SPELEND) {
+                            clients[j].status = WACHTEND
+                        }
+                        else if (clients[j].status === GEKICKT) {
+                            clients[j].status = AANMELDEND
+                        }
+                    }
+                }
+                break
             }
         }
+        delete clients[i]
+        clients = clients.filter(x => x) //remove empty slots
+        scherm_wachtend_of_aanmeldend_sturen()
     })
     ws.onerror = () => {
         console.log("Some error occurred")
--- public/script/script.js
+++ public/script/script.js
@@ -1,9 +1,12 @@
+let update = true
 ws.addEventListener('message', function (event){
     console.log("message reveived" + event.data)
     message = JSON.parse(event.data)
     //document.getElementById(message.id).innerHTML = message.htmlFragment
-    const target = document.getElementById(message.id)
-    target.innerHTML = ''  // clear the original content
-    const new_content = document.createRange().createContextualFragment(message.htmlFragment);
-    target.append(new_content);
+    if (update){
+        const target = document.getElementById(message.id)
+        target.innerHTML = ''  // clear the original content
+        const new_content = document.createRange().createContextualFragment(message.htmlFragment);
+        target.append(new_content);
+    }
 })
